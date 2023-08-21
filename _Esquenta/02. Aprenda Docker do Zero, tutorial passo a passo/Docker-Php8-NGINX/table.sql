51eaa5f368616c4a78652a023bde0e1f2a52e9d9
Vladimir Pezzole <vladimir.hipermidia@gmail.com>
2023-08-21 13:37:43 -0300

    Update files

--- _Esquenta/02. Aprenda Docker do Zero, tutorial passo a passo/Docker-Php8-NGINX/table.sql
+++ _Esquenta/02. Aprenda Docker do Zero, tutorial passo a passo/Docker-Php8-NGINX/table.sql
@@ -1,4 +1,1 @@
-CREATE TABLE `labphp8-db`.`Teste` (`id` INT(11) NOT NULL AUTO_INCREMENT , `name` VARCHAR(100) NOT NULL , `sobrenome` VARCHAR(100) NOT NULL , `tel` VARCHAR(20) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-INSERT INTO `Teste`(`name`, `sobrenome`, `tel`) VALUES ('João','Silva','99996666');
-INSERT INTO `Teste`(`name`, `sobrenome`, `tel`) VALUES ('Maria','Silva','96969696');
-INSERT INTO `Teste`(`name`, `sobrenome`, `tel`) VALUES ('Francisca','Souza','99669966');
+/media/vladimir/Myfiles/_SERVER/www/_Lab/_workshops/FullCycle/_Uteis/table.sql
